b7091fb94d86aad25d3a15649c595a67de1a645d
Timothy Tran <tttran@vt.eu>
2017-02-18 12:07:01 -0500

    random commit

--- test/index.js
+++ test/index.js
@@ -45,26 +45,25 @@ request
     }
 });*/
 
-function getAccount(customerID) {
-  var getAccountsUrl = 'http://api.reimaginebanking.com/customers/'.concat(customerID).concat('/accounts?key=').concat(apikey);
-  request
-    .get(getAccountsUrl)
-    .end(function(err, res) {
-      // Calling the end function will send the request 
-      if (err) console.log(err);
-      //console.log(res.status);
-      console.log(res.body);
-    for(var i in res.body) {                    
-      if(res.body[i].type === 'Checking') {
-        var accountID = res.body[i]._id;
-        console.log('Account ID: ' + accountID);
-        /*return accountID;*/
-      }
-    }
-  });
+function getAccount(callback, customerID) {
+  var hi;
+  var getAccountsUrl = 'http://api.reimaginebanking.com/customers/'.concat(customerID).concat('/accounts?key=').concat(apikey); 
+    request.get(getAccountsUrl).end(function(err, res) {
+        if (err) return err.body;
+        callback(res.body);
+      });
 }
 
-getAccount('56c66be5a73e492741507429');
+function accountHandler(result) {
+  for(var i in result) {                    
+    if(result[i].type === 'Checking') {
+        console.log('Account ID: ' + result[i]._id);
+        break;
+    }
+  }
+}
+
+getAccount(accountHandler, '56c66be5a73e492741507429');
 
 function transfers(senderAccountID, recieverAccountID, amount) {
   var postTransfersUrl = 'http://api.reimaginebanking.com/accounts/'.concat(senderAccountID).concat('/transfers?key=').concat(apikey)
@@ -75,7 +74,7 @@ function transfers(senderAccountID, recieverAccountID, amount) {
       "medium": "balance",
       "payee_id": recieverAccountID,
       "amount": amount,
-      "transaction_date": "2017-02-18",
+      "transaction_date": getDate(),
       "description": "transfer_test"
     })
     .end(function(err, res) {
@@ -85,5 +84,20 @@ function transfers(senderAccountID, recieverAccountID, amount) {
     });
 }
 
-transfers(myCustomerID, '56c66be6a73e492741507f64', 0.01);
+//transfers(myCustomerID, '56c66be6a73e492741507f64', 0.01);
+
+function getDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+
+  var yyyy = today.getFullYear();
+  if(dd<10){
+    dd='0'+dd;
+  } 
+  if(mm<10){
+    mm='0'+mm;
+  } 
+  return yyyy + '-' + mm + '-' + dd;
+}
 
