1025b06ab4879a5b6de987e28eccc29ddc4293a8
Jacob Contreras <jacob6@vt.edu>
2017-02-17 23:00:20 -0500

    added read and store functionality

--- src/database_helper.js
+++ src/database_helper.js
@@ -19,6 +19,42 @@ var databaseTable = function(){
   return dynasty.table(DATABASE_TABLE_NAME);
 };
 
+// DatabaseHelper.prototype.createDatabaseTable = function() {
+//   return dynasty.describe(DATABASE_TABLE_NAME).catch(function(err){
+//     console.log('createDatabaseTable, err: ', err);
+//     return dynasty.create(DATABASE_TABLE_NAME, {
+//       key_schema: {
+//         hash: ['userId', 'string']
+//       }
+//     });
+//   });
+// };
+
+DatabaseHelper.prototype.storeData = function(userId, data){
+  console.log('writing to database user: ', userId);
+  return databaseTable().insert({
+    userId: userId,
+    data: JSON.stringify(data)
+  }).catch(function(err){
+    console.log(err);
+  });
+};
+
+DatabaseHelper.prototype.readData = function(userId){
+  console.log('reading data for user: ' + userId);
+  return databaseTable().find(userId).then(function(result){
+    var data;
+    if(result === undefined){
+      data = {};
+    }else{
+      data = JSON.parse(result['data']);
+    }
+    return new DatabaseHelper(data);
+  }).catch(function(err){
+    console.log(err);
+  });
+};
+
 
 
 module.exports = DatabaseHelper;
