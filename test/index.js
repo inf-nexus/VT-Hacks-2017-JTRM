c5314996a64a9f2632df5951f22e6beab7e460c9
rohanrane <rohanr@vt.edu>
2017-02-18 00:28:29 -0500

    Added functionality for getting accountID

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 var request = require('superagent');
 
+var apikey = '1e28a437bbeb9528ba382435b7e2abe1';
+
 /*request
   .post('http://api.reimaginebanking.com/customers?key=1e28a437bbeb9528ba382435b7e2abe1')
   .send({
@@ -21,12 +23,44 @@ var request = require('superagent');
       console.log(res.body);
   });*/
 
-
+//Grabbing accounts
+/*var accountID;
+var customerID = '58a7b1b01756fc834d904a02';
+var getAccountsUrl = 'http://api.reimaginebanking.com/customers/'.concat(customerID).concat('/accounts?key=').concat(apikey);
 request
-  .get('http://api.reimaginebanking.com/accounts?key=1e28a437bbeb9528ba382435b7e2abe1')
+  .get(getAccountsUrl)
   .end(function(err, res){
     // Calling the end function will send the request 
     if (err) console.log(err);
-      console.log(res.status);
+      //console.log(res.status);
       console.log(res.body);
-  });
+    for(var i in res.body) {                    
+      if(res.body[i].type === 'Checking') {
+        accountID = res.body[i]._id;
+        console.log('Account ID: ' + accountID);
+      }
+    }
+});*/
+
+
+  function transfer(sender_accnt_id, reciever_accnt_id, amnt) {
+  request
+    .post('http://api.reimaginebanking.com/transfers?key=b7b749abc5269fb91882402aad541b37')
+    .set('Content-Type', 'application/json')
+    .set('id', sender_accnt_id)
+    .send({
+      "medium": "balance",
+      "payee_id": reciever_accnt_id,
+      "amount":amnt,
+      "transaction_date": "2017-02-18",
+      "description": "transfer_test"
+    })
+    .end(function(err, res) {
+      if (err) console.log(err);
+      console.log(res.status);
+      console.log(res.status);
+    });
+}
+
+transfer('56c66be6a73e492741507f63', '56c66be6a73e492741507f64', 0.01);
+
